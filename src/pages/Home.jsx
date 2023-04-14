11c13c0a95ff125139e9ad661940332d91b0afa5
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-04-14 23:21:43 +0300

    style: edited home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,50 @@
+import { auth } from "../firebase";
+import { useSignOut } from "react-firebase-hooks/auth";
+import { Box, Typography, Button } from "@mui/material";
+import Loading from "../components/Loading";
+
 const Home = () => {
-  return <div>Home</div>;
+  const [signOut, loading] = useSignOut(auth);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  const handleSignOut = () => {
+    signOut();
+    alert("You logged out!");
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 2,
+      }}
+    >
+      <Typography
+        variant="h5"
+        sx={{
+          color: "white",
+          fontWeight: 700,
+          textTransform: "uppercase",
+        }}
+      >
+        You logged in!
+      </Typography>
+      <Button
+        onClick={handleSignOut}
+        color="success"
+        variant="contained"
+        size="large"
+      >
+        Sign out
+      </Button>
+    </Box>
+  );
 };
 
 export default Home;
